b759dbc33a46c090df54d11c989a585277a0a2c9
shimoda <kuri65536@hotmail.com>
2020-07-26 22:05:53 +0000

    add a test for colspan.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='cell_row_span',
       version='1.0',
@@ -6,4 +6,6 @@ setup(name='cell_row_span',
                   'in tables.',
       url='https://github.com/Neepawa/cell_row_span',
       py_modules=['cell_row_span'],
+      require=["markdown"],
+      test_suite='tests',
       )
---
+++ tests/colspan.py
@@ -0,0 +1,41 @@
+from markdown.test_tools import TestCase
+from cell_row_span import CellRowSpanExtension
+
+
+class colspan(TestCase):
+    def runTest(self):
+        src = self.dedent("""
+        c11 | c12 | c13
+        ----|-----|-----
+        c21      || c22
+        c31 | c32 | c33
+        """)
+        exp = self.dedent("""
+        <table>
+        <thead>
+        <tr>
+        <th>c11</th>
+        <th>c12</th>
+        <th>c13</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+        <td colspan="2">c21</td>
+        <td>c22</td>
+        </tr>
+        <tr>
+        <td>c31</td>
+        <td>c32</td>
+        <td>c33</td>
+        </tr>
+        </tbody>
+        </table>
+        """)
+        self.assertMarkdownRenders(src, exp, output_format="html",
+                                   extensions=['tables',
+                                               CellRowSpanExtension()])
+
+
+# class (TestCase, colspan):
+#     pass
